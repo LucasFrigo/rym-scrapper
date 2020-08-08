6d04706a429347e310c337fd44195a4eba02b186
Lucas Guilherme <lukeg299@gmail.com>
2020-08-08 15:46:56 -0300

    Initial commit

---
+++ src/main/java/io/frigo/lucas/rymscrapper/RymScrapperApplication.java
@@ -0,0 +1,13 @@
+package io.frigo.lucas.rymscrapper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RymScrapperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RymScrapperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/frigo/lucas/rymscrapper/RymScrapperApplicationTests.java
@@ -0,0 +1,13 @@
+package io.frigo.lucas.rymscrapper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RymScrapperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
